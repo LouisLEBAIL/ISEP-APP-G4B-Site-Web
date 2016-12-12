68a613a77b6c2ff6b6e6fe1af1a0d7be3a552c09
Appg4blc <l.chabert8@laposte.net>
2016-12-12 16:53:35 +0100

    Ajout brouillon

---
+++ brouillon/Etat des fonctionnalités par pièces.php
@@ -0,0 +1,76 @@
+Etat des fonctionnalités par pièces.php
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" /> <link rel="stylesheet" href="Page_service_client.css" />
+
+        <title>Page de connexion</title>
+
+        <title>Présentation de DomLab</title>
+
+</head>
+    	<body>
+    		<div id="bloc_page_4">
+                <?php include("en_tete.php");?>
+                <div id="container_4">
+                <?php include("navigation_client.php");?>
+
+                    
+            <table id="tableau" border="1">
+            <caption>Description pièce par pièce</caption>
+            <tr>
+              <thead><td>Pièce 
+            Salon
+            Sejour
+            Cuisine
+            Cellier 
+            Garage
+            Bureau
+            Entrée
+            Chambres (1,2,3)
+            Salles de bain (2)
+</td>
+              <td>fonctionnalités par pièces</td></thead>
+            </tr>
+            <tr>
+              <tbody><td>Luminosité</td>
+              <td>valeur</td>
+            </tr>
+            <tr>
+              <td>Batterie capteur</td>
+              <td colspan="3">Faible/Normale/Pleine</td>
+            </tr>
+            <tr>
+              <td>température</td>
+              <td>valeur</td>
+            </tr>
+            <tr>
+              <td>Batterie capteur</td>
+              <td colspan="3">Faible/Normale/Pleine</td>
+
+            <p>Ouverture/Fermeture des volets</p>
+
+
+            <p>Ouverture/fermeture des volets</p>
+
+ <table id="tableau" border="1">
+            <caption>Programmation chauffage</caption>
+            <tr>
+             <td>Nuit</td>
+             <td>Matin</td>
+             <td>Après-midi</td>
+             <td>Soir</td>
+           
+
+
+
+<?php include("pied_de_page.php");?>
+            </div>
+        </body>
+</html>
+
+
+
---
+++ brouillon/Page service client.php
@@ -0,0 +1,217 @@
+Page service client 
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" /> <link rel="stylesheet" href="Page_service_client.css" />
+
+        <title>Page de connexion</title>
+
+        <title>Présentation de DomLab</title>
+
+    </head>
+    	<body>
+    		<div id="bloc_page_4">
+                <?php include("en_tete.php");?>
+                <div id="container_4">
+                <?php include("navigation_client.php");?>
+
+                    <form method="post" action="#">
+
+                
+                    
+
+                    <p>Votre adresse e-mail</p>
+                      <fieldset>
+                            <p>
+                                <label for="email">Adresse email:</label>
+                            </p>
+                            <p>
+                                <input type="text" name="email">
+                            </p>
+
+
+
+            <input type="submit" value="Envoyer" />
+
+
+
+            
+            <p>Veuillez renseigner les champs suivants pour acceder à vos données personnelles</p>
+
+                    <form action="#" method="post">
+
+                      <fieldset>
+                            <p>
+                                <label for="Nom">Nom:</label>
+                            </p>
+                            <p>
+                                <input type="text" name="Nom" required="">
+                            </p>
+                            <p>
+                                <label for="Prenom">Prenom:</label>
+                            </p>
+                            <p>
+                                <input type="text" name="Prenom" required="">
+                            </p>
+                            <p>
+                                <label for="Nombre_de_capteurs">Nombre de capteurs:</label>
+                            </p>
+                            <p>
+                                <input type="number" name="Nombre_de_capteurs" required="">
+                            </p> 
+                            <p>
+                                <label for="ID_client">ID client:</label>
+                            </p>
+                            <p>
+                                <input type="number" name="ID_client" required="">
+                            </p>
+                            <p>
+                                 <label for="Durée_inscription">Client depuis le:</label>
+                            </p>
+                            <p> 
+                                <input type="date" name="date" required="">
+                            <p>
+                                <input type="submit" value="Envoyer" />
+                            
+                            </p>
+            
+
+
+<?php 
+
+try 
+{
+  //On se connecte à MYSQL
+  $bdd = new PDO('mysql:host=localhost;dbname=ISEP-APP-G4B-Site-Web;charset=utf8', 'root', '');
+
+}
+// On récupère tout le contenu de la table jeux_video
+$reponse = $bdd->query('SELECT * FROM etat_des_fonctionnalités');
+
+while ($donnees = $reponse->fetch())
+{
+  ?>
+
+            <table id="tableau" border="1">
+            <caption><strong>Etat des fonctionnalités</strong></caption><?php echo $donnees['Etat_des_fonctionnalites']; ?><br/>
+            Les fonctionnalités suivantes <?php echo $donnees['Fonctionnalités']; ?> sont : <?php echo $données['Etat']; ?> fonctionnelles, non fonctionelles ou il n'y a rien à signaler.
+
+            <tr>
+              <thead><td>Fonctionnalités</td>
+              <td>Etat</td></thead>
+            </tr>
+            <tr>
+              <tbody><td>Luminosité</td>
+              <td colspan="3">RAS/Fonctionnement partiel/Non fonctionnel</td>
+            </tr>
+            <tr>
+              <td>Température</td>
+              <td colspan="3">RAS/Fonctionnement partiel/Non fonctionnel</td>
+            </tr>
+            <tr>
+              <td>détecteur de mouvements</td>
+              <td colspan="3">RAS/Fonctionnement partiel/Non fonctionnel</td>
+            </tr>
+            <tr>
+              <td>Incendie</td>
+              <td colspan="3">RAS/Fonctionnement partiel/Non fonctionnel</td>
+              </tr>
+              <tr>
+              <td>Détecteur de présence (caméra)</td>
+              <td colspan="3">RAS/Fonctionnement partiel/Non fonctionnel</td></tbody>
+              </tr>
+            </table>
+<?php 
+{
+  $reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
+
+           
+
+<?php 
+
+try 
+{
+  //On se connecte à MYSQL
+  $bdd = new PDO('mysql:host=localhost;dbname=ISEP-APP-G4B-Site-Web;charset=utf8', 'root', '');
+
+}
+// On récupère tout le contenu de la table jeux_video
+$reponse = $bdd->query('SELECT * FROM etat_des_fonctionnalités');
+
+while ($donnees = $reponse->fetch())
+{
+  ?>
+
+            <table id="tableau" border="1">
+            <caption><strong>Vue globale client en cours<strong></caption><?php echo $données['Numero_de_pieces']; ?><br/>
+            Vous avez <?php echo $donnees['Numero_de_pieces']; ?> pièces qui sont : <?php echo $donnees['Pieces']; ?>
+            <tr>
+              <thead><td>Numéro de pièces</td>
+              <td>Pièces</td></thead>
+            </tr>
+            <tr>
+              <tbody><td>1</td>
+              <td>Salon</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Séjour</td>
+            </tr>
+              <td>3</td>
+              <td>Cuisine</td>
+            </tr>
+              <td>4</td>
+              <td>Cellier</td>
+            </tr>
+              <td>5</td>
+              <td>Garage</td>
+            </tr>
+              <td>6</td>
+              <td>Bureau</td>
+            </tr>
+            <td>7</td>
+              <td>Entrée</td>
+            </tr>
+              <td>8</td>
+              <td colspan="3">Chambre 1/Chambre 2/Chambre 3</td>
+            </tr>
+              <td>9</td>
+              <td colspan="2">Salles de bain RDC/Salle de bain étage)</td>
+            </tr>
+            </tbody>
+            </table>
+
+<?php 
+{
+  $reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
+
+
+   
+<?php include("pied_de_page.php");?>
+            </div>
+        </body>
+</html>
+
+
+          
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
