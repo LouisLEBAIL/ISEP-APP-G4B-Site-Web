cf3be2b0f87e08a6dbda53e2bcb59e2ea683d95f
Appg4blc <l.chabert8@laposte.net>
2017-01-10 12:34:43 +0100

    modifs page client travail ds brouillon

--- brouillon/page_client_travail_de_qualite.php
+++ brouillon/page_client_travail_de_qualite.php
@@ -2,6 +2,10 @@
 $volet_1=true;
 $volet_2=true;
 $volet_3=true;
+$chauffage_1=true;
+$chauffage_2=true;
+$chauffage_3=true;
+$batterie_capteur=true;
 $securite_1=true;
 $securite_2=true;
 $securite_3=true;
@@ -31,6 +35,8 @@ if
                     <div id="lien_donnees">
                         <a href="temperature">Température</a>
                         <a href="luminosite">Luminosité</a>
+                        <a href="humidite">Humidité</a>
+                        <a href="mouvements">Mouvements</a>
                         <a href="autres">Autres</a>
                     </div>  
                     <table border="1" id="securite">
@@ -78,6 +84,10 @@ if
                                 {
                                     echo "volet fermé";
                                 }
+                                else
+                                {
+                                    echo "volets defectueux";
+                                }
                                 ?>
                             </td>
                         </tr>
@@ -96,6 +106,10 @@ if
                                 else
                                 {
                                     echo "volet fermé";
+                                }
+                                else
+                                {
+                                    echo "volets defectueux";
                                 }
                                 ?>
                             </td>
@@ -117,11 +131,123 @@ if
                                 {
                                     echo "volet fermé";
                                 }
+                                else
+                                {
+                                    echo "volets defectueux";
+                                }
                                 ?>
                             </td>
                         </tr>
                     </table>
 }
+
+
+<table border="1" id="chauffage">
+                        <caption>Chauffage</caption>
+                        <tr>
+                            <td>
+                                <?php
+                                    echo $donnees['nom_piece']
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                if ($chauffage_1==true)
+                                {
+                                    echo "chauffage allume";
+                                }
+                                else
+                                {
+                                    echo "chauffage eteint";
+                                }
+                                else
+                                {
+                                    echo "chauffage defectueux";
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <?php
+                                    echo $donnees['nom_piece']
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                if ($chauffage_2==true)
+                                {
+                                    echo "chauffage allume";
+                                }
+                                else
+                                {
+                                    echo "chauffage eteint":
+                                }
+                                else
+                                {
+                                    echo"chauffage defectueux";
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <?php
+                                    echo $donnees['nom_piece']
+                                ?>
+                                </td>
+                                <td>
+                                <?php
+
+                                if ($chauffage_3==true)
+                                {
+                                    echo "chauffage allume";
+                                }
+                                else
+                                {
+                                    echo"chauffage eteint";
+                                }
+                                else
+                                {
+                                    echo"chauffage defectueux";
+                                }
+                                ?>
+                            </td>
+                        </tr>
+
+<table border="1" id="batterie_capteur">
+                        <caption>Batterie Capteur</caption>
+                        <tr>
+                            <td>
+                                <?php
+                                    echo $donnees['nom_piece']
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                if ($batterie_capteur==true)
+                                {
+                                    echo "batterie pleine";
+                                }
+                                else
+                                {
+                                    echo "batterie endommagée";
+                                }
+                                else
+                                {
+                                    echo "batterie vide";
+                                }
+                                ?>
+                            </td>
+                        </tr>
+
+
+
+
+
+
+
+
                 </div>
                 <?php include("pied_de_page.php");?>
             </div>
