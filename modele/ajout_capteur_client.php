ec3eab849453ffbc33dd3d13d17361dc347d1f95
Endrrick <erapa.endrick@gmail.com>
2017-01-16 17:04:06 +0100

    dernier changemnt

--- modele/ajout_capteur_client.php
+++ modele/ajout_capteur_client.php
@@ -8,13 +8,20 @@ $requser = $bdd -> prepare('SELECT * FROM client WHERE id_client=?');
 $requser -> execute(array($_SESSION['id_client']));
 
 
-if(isset($_POST['validercapteur']))
+$numero_capteur=1;
+
+
+if(isset($_POST['validercapteur']) AND !empty($_POST['numero_de_serie']))
 {
 	$numeroseriecapteur=htmlspecialchars($_POST["numero_de_serie"]);
 	$reqnumeroseriecapteur=$bdd->prepare('SELECT * FROM numero_serie_capteur WHERE numero_serie_capteur=?');
 	$reqnumeroseriecapteur->execute(array($numeroseriecapteur));
 	$numeroserieexist=$reqnumeroseriecapteur->rowCount();
-	if($numeroserieexist==1)
+	$reqcapteur = $bdd -> prepare('SELECT * FROM capteur WHERE id_client=? AND numero_serie_capteur=?');
+$reqcapteur -> execute(array($_SESSION['id_client'],$_POST["numero_de_serie"]));
+
+	$numeroseriedejautiliser=$reqcapteur->rowCount();
+	if($numeroserieexist==1 AND !empty($_POST['numero_de_serie']) AND $numeroseriedejautiliser==0 )
 	{
 		$a=0;
 
@@ -22,19 +29,21 @@ if(isset($_POST['validercapteur']))
 
 		$type2=type_capteur($numeroseriecapteur);
 
-		$reqcapteur=$bdd->prepare('INSERT INTO capteur(type, numero_serie_capteur, etat, id_piece, id_client) VALUES(:type, :numero_serie_capteur, :etat, NULL, :id_client)');
+		$reqcapteur=$bdd->prepare('INSERT INTO capteur(type, numero_serie_capteur, numero_capteur, etat, id_piece, id_client) VALUES(:type, :numero_serie_capteur, :numero_capteur, :etat, NULL, :id_client)');
 		$reqcapteur->execute(array(
     		'type'=>$type2,
     		'numero_serie_capteur'=>$numeroseriecapteur,
+    		'numero_capteur'=>$numero_capteur,
     		'etat'=>$a,
     		'id_client'=>$_SESSION['id_client']
 		));
-		$erreur="Capteur ajouter";
+		$erreur="Capteur ajouté";
 	}
-	else
+	elseif (empty($_POST['numero_de_serie']))
 	{
-    	$erreur="probleme";
+    	$erreur="Numero de série du capteur incorect";
 	}
+
 }
 
 ?>
