e793101af714a3bd07e18285358380f5c71b79fb
LouisLEBAIL <louislebail96@gmail.com>
2017-01-16 22:03:26 +0100

    Debut nouvelles page d accueil

--- modele/page_accueil_client.php
+++ modele/page_accueil_client.php
@@ -2,15 +2,23 @@
 
 include ("connexion_bdd.php");
 
+$req_piece = $bdd->prepare('SELECT nom_piece FROM piece WHERE id_client=?');
+$req_piece->execute(array($_SESSION['id_client']));
 
-$reponse1 = $bdd->prepare('SELECT nom_piece FROM piece WHERE id_client=?');
-$reponse1->execute(array($_SESSION['id_client']));
+/* TYPES DE CAPTEURS PAR SALLE */
 
-$reponse2 = $bdd->prepare('SELECT nom_piece FROM piece WHERE id_client=?');
-$reponse2->execute(array($_SESSION['id_client']));
+while ($toutes_les_pieces = $req_piece -> fetch())
+{
+	$req_id_pieces = $bdd->prepare('SELECT id_piece FROM piece WHERE id_client=? AND nom_piece=?');
+	$req_id_pieces->execute(array($_SESSION['id_client'],$toutes_les_pieces['nom_piece']));
 
-$etat = $bdd->prepare('SELECT etat FROM capteur WHERE id_client=?');
-$etat->execute(array($_SESSION['id_client']));
-$etat_capteur = $etat->fetch();
+	while ($id_des_pieces = $req_id_pieces -> fetch())
+	{
+		$req_type_capteur = $bdd->prepare('SELECT type FROM capteur WHERE id_client=? AND id_piece=?');
+		$req_type_capteur->execute(array($_SESSION['id_client'],$id_des_pieces['id_piece']));
+
+		while ($type_de_capteur = $req_type_capteur -> fetch())	
+	}
+}
 
 ?>
--- vue/page_accueil_client.php
+++ vue/page_accueil_client.php
@@ -6,38 +6,34 @@
         <title>Page d'accueil</title>
     </head>
     <body>
-		<?php include("vue/en_tete_client.php");?>
+        <?php include("vue/en_tete_client.php");?>
+    <div id="container_2">
+            <?php include("vue/navigation_client.php");?>
+        </div>
 
+    <?php
+    while ($toutes_les_pieces = $req_piece -> fetch())
+    {
+    ?>
+        <div class=tous_les_encadres>
+            <div class=tous_les_types>
+                <div class=nom_piece>
+                    <?php echo $toutes_les_pieces['nom_piece']; ?>
+                </div>
+                
 
-		<div id="container_2">
-			<?php include("vue/navigation_client.php");?>
-			<div id="lien_donnees">
-            	<a href="temperature">Température</a>
-            	<a href="luminosite">Luminosité</a>
-            	<a href="autres">Autres</a>
             </div>
+        </div>
+    <?php
+    }
+    ?>
 
-        	<div id='table1'> 
-        	<caption>Volets</caption>
-        	<table border="1">
-            <tr>
-                <th>Pièce</th>
-                <th>Volets Ouverts/Fermés</th>
-                <th>Position des volets</th>
-                <th>Normal ?</th>
-                <th>Etat des</br>batteries</th>
-            </tr>
+        <?php include("vue/pied_de_page.php");?>
+    </body>
+</html>
 
-                <?php
-                $i=0;
-            while ($piece = $reponse1->fetch())
-            {
-                $i++;
-                ?>
-                <tr>
-                    <th><?php echo $piece['nom_piece'] ;?></th>
-                    <td>
-                        <?php
+<?php
+/*
                         echo'<div class="onoffswitch">';
                         echo'<input type="checkbox" name="onoffswitch[]" class="onoffswitch-checkbox" id="myonoffswitch'.$i.'" value="valeur">';
                         echo'<label class="onoffswitch-label" for="myonoffswitch'.$i.'">';
@@ -47,133 +43,5 @@
                         echo'</span>';
                         echo'</label>';
                         echo '</div>';
-                        ?>
-                    </td>            
-                    <td>
-                        <?php
-                        $j = rand (0,1) ;
-                        if ($j == 0)
-                        {
-                            echo 'Ouvert';
-                        }
-                        else
-                        {
-                            echo 'Fermé';
-                        }
-                        ?>
-                    </td>
-                    <td class=pre_circle>
-                        <?php
-                        $j = rand (0,9) ;
-                        if ($j == 0)
-                        {
-                            ?>
-                            <div class=circle_red></div>
-                            <?php
-                        }
-                        elseif ($j == 1 or $j==2)
-                        {
-                            ?>
-                            <div class=circle_orange></div>
-                            <?php
-                        }
-                        else
-                        {
-                            ?>
-                            <div class=circle_green></div>
-                            <?php
-                        }
-                        ?>
-                    </td>
-                    <td class=pre_circle>
-                        <?php
-                        if ($etat_capteur['etat'] == 3)
-                        {
-                            ?>
-                            <div class=circle_red></div>
-                            <?php                            
-                        }
-                        elseif ($etat_capteur['etat'] == 2)
-                        {
-                            ?>
-                            <div class=circle_orange></div>
-                            <?php
-                        }
-                        else
-                        {
-                            ?>
-                            <div class=circle_green></div>
-                            <?php
-                        }
-                        ?>
-                    </td>
-                </tr>
-            <?php
-            }
-            ?>
-        	
-        	</table>
-        	</div>
-
-        	<div id='table2'>
-        	<caption>Sécurité</caption>
-        	<table border="1">
-            <tr>
-                <th>Pièce</th>
-                <th>Niveau</br>d'alerte</th>
-                <th>Normal ?</th>
-            </tr>
-
-        	<?php
-        	while ($pieces = $reponse2->fetch())
-        	{
-        		?>
-        		<tr>
-        			<th><?php echo $pieces['nom_piece'] ;?></th>
-        			<td>
-        				<?php
-        				$j = rand (0,4) ;
-        				if ($j == 0)
-        				{
-        					echo 'Alerte sécurité';
-        				}
-        				else
-        				{
-        					echo 'Aucun problème de sécurité';
-        				}
-        				?>
-        			</td>
-                    <td class=pre_circle>
-                        <?php
-                        $j = rand (0,9) ;
-                        if ($j == 0)
-                        {
-                            ?>
-                            <div class=circle_red></div>
-                            <?php
-                        }
-                        elseif ($j == 1 or $j==2)
-                        {
-                            ?>
-                            <div class=circle_orange></div>
-                            <?php
-                        }
-                        else
-                        {
-                            ?>
-                            <div class=circle_green></div>
-                            <?php
-                        }
-                        ?>
-                    </td>
-        		</tr>
-        		<?php
-        	}
-        	?>
-        	</table>
-        	</div>
-		</div>
-		<?php include("vue/pied_de_page.php");?>
-	</body>
-</html>
-                                                       
+*/
+?>
