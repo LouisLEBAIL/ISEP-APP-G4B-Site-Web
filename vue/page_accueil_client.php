d39b7507fed469766e91831d27878e5f6071b279
LouisLEBAIL <louislebail96@gmail.com>
2017-01-23 20:28:52 +0100

    Nouvelle BDD

--- vue/page_accueil_client.php
+++ vue/page_accueil_client.php
@@ -12,17 +12,60 @@
         </div>
 
     <?php
-    while ($toutes_les_pieces = $req_piece -> fetch())
+    $req_piece1 = $bdd->prepare('SELECT * FROM piece WHERE id_client=?');
+$req_piece1->execute(array($_SESSION['id_client']));
+    while ($toutes_les_pieces = $req_piece1 -> fetch())
     {
     ?>
-        <div class=tous_les_encadres>
-            <div class=tous_les_types>
-                <div class=nom_piece>
+        <div class='tous_les_encadres'>
+            <div class='tous_les_types'>
+                <div class='nom_piece'>
                     <?php echo $toutes_les_pieces['nom_piece']; ?>
                 </div>
-                
+                <div class='tous_les_capteurs'>
+                    <?php
+                    while ($b = $req_piece1 -> fetch()) // boucle pour toutes les pieces
+                    {
+                        $req1 = $bdd->prepare('SELECT * FROM capteur WHERE id_client=? AND id_piece=?');
+                        $req1->execute(array($_SESSION['id_client'],$b['id_piece']));
 
+                        while ($c = $req1 -> fetch()) // boucle pour tous les capteurs par piece
+                        {
+                            // Requetes a la base de donnee
+                            $req2 = $bdd->prepare('SELECT * FROM capteur WHERE  id_capteur=?');
+                            $req2->execute(array($c['id_capteur']));
+
+
+                            // Remplissage des variables a afficher par pices et par capteurs
+                            while ($z = $req1 -> fetch()) // Sortie: id de la piece ou est le capteur
+                            {
+                                $id_de_la_piece = $z['id_piece']; 
+                            }
+
+                            while ($a = $req2 -> fetch()) // Sortie: variable ou est l etat du capteur & type de capteur
+                            {
+                                $type_du_capteur = $a['type'];
+                            }
+
+                            
+                                ?>
+                                <div class='capteurs'>
+                                <?php
+
+                                echo $type_du_capteur.$id_de_la_piece;
+
+                                substr($type_du_capteur.$id_de_la_piece,0)
+
+                                ?>
+                                </div>
+                                <?php
+                                                                      
+                        }
+                    }
+                    ?>
+                </div>
             </div>
+            <div class='bouton_de_synchronisation'></div>
         </div>
     <?php
     }
