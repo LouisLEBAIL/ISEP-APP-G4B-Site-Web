faa51b98d6876fb39d08278d1dc206b82221def8
Eva Seck <evgseck@yahoo.fr>
2017-01-23 12:56:01 +0100

    advancement

---
+++ vue/en_tete_service_client.php
@@ -0,0 +1,14 @@
+<header>
+    <div id="favicon">
+
+                     <link rel="icon" type="image/gif" href="picture/favicon.gif">
+
+                     <link rel="icon" type="image/gif" href="image/favicon.gif">
+
+    <div id="DHomeLab">
+        <div id="logo">
+            <a href="index.php?redirection=connecte"><img  src="../picture/logo_transparent.png" alt="logo DomLab" title="DomLab"/></a>
+        </div>
+        <h2 class="titre">DHomeLab</h2>
+        <h4><a href="index_service_client.php?redirection=deconnexion">Déconnexion</a></h4>
+</header>
