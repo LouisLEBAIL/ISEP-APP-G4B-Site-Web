6b0a1c47f524dfd4b74e410881f639bb2906ddcd
TheSwordMasta <eric.s.v@hotmail.fr>
2017-01-26 10:58:44 +0100

    mot de passe oublié

---
+++ modele/mot_de_passe_oublie.php
@@ -0,0 +1,8 @@
+
+<?php
+	$verification_email = $bdd -> query ('SELECT numero_serie_ceMAC from client WHERE email = $POST_email')
+	if ($POST_numero_serie_ceMAC == $numero_serie_ceMAC)
+	{
+		
+	}
+?>
