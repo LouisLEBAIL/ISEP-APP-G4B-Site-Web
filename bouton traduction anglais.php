b297090baec12ba68d0d6b590f4fec3ed0453665
Appg4blc <l.chabert8@laposte.net>
2017-02-03 10:50:43 +0100

    site anglais

--- bouton traduction anglais.php
+++ bouton traduction anglais.php
@@ -1,7 +1,20 @@
 /* bouton trad anglais*/
 
 
+<header>
+    <div id="favicon">
 
+                     <link rel="icon" type="image/gif" href="picture/favicon.gif">
+
+                     <link rel="icon" type="image/gif" href="image/favicon.gif">
+
+    <div id="DHomeLab">
+        <div id="logo">
+            <a href="index.php?redirection=connecte"><img  src="../picture/logo_transparent.png" alt="logo DomLab" title="DomLab"/></a>
+        </div>
+        <h2 class="titre">DHomeLab</h2>
+        <h4><a href="index_service_client.php?redirection=bouton traduction anglais">English website</a></h4>
+</header>
 
 
 
