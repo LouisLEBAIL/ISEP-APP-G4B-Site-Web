659deb91bfb02311cce7d00915f36f770e3a0de0
Appg4blc <l.chabert8@laposte.net>
2017-02-06 17:13:48 +0100

    :;

--- bouton traduction anglais.php
+++ bouton traduction anglais.php
@@ -5,28 +5,18 @@
 <html>
 
 
-<header>
-    <div id="favicon">
-
-                     <link rel="icon" type="image/gif" href="picture/favicon.gif">
-
-                     <link rel="icon" type="image/gif" href="image/favicon.gif">
-
-    <div id="DHomeLab">
-        <div id="logo">
-            <a href="index.php?redirection=connecte"><img  src="../picture/logo_transparent.png" alt="logo DomLab" title="DomLab"/></a>
- 
 
     <head>
     <meta charset="utf-8" /> 
         <link rel="stylesheet" href="style/bouton_traduction.css" />
        <div>
-        <h2 class="titre">DHomeLab</h2>
-        <h4><a href="index_client.php?redirection=boutontraductionanglais">English website</a></h4>
+        <h1><a href="index_client.php?redirection=boutontraductionanglais">English website</a></h1>
 </div>
 </head>
-<?php
 
+<?php
+include 'vue/en_tete_client.php';
+            
 include('vue/page_accueil_client.php');
 
 ?>
